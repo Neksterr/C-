8941f61c4a54d9c95c740d16803196679cb35622
Nekster <nekster187@gmail.com>
2018-11-30 01:07:59 +0200

    Update program1.cpp

--- ks2/program1.cpp
+++ ks2/program1.cpp
@@ -4,29 +4,31 @@ using namespace std;
 void main()
 {
 	//L - отсечка, R - процент
-	int n, x[20], y[20], i, S = 0, SrArr = 0, br = 0, L, R;
+	int n, x[20], y[20], i, br = 0;
+		float S = 0, SrArr = 0,  L[20], R , c;
 
 	cout << "n = ";
 	cin >> n;
 
 	for ( i = 0; i < n; i++)
 	{
-		cout << "x[ " << x[i] << "]\n";
+		cout << "x[ " <<i<< "]=";
 		cin >> x[i];
 	}
 	for (i = 0; i < n; i++)
 	{
-		cout << "y[ " << y[i] << "]\n";
+		cout << "y[ " <<i<< "]=";
 		cin >> y[i];
 	}
 
-	for ( i = 0; i <= n - 1; i++)
+	for ( i = 0; i < n - 1; i++)
 	{
-		L[i] = sqrt(((x[i] + 1) - x[i])*((x[i] + 1) - x[i]) + ((y[i] + 1) - y[i])*((y[i] + 1) - y[i])) ;
+		c = (x[i + 1] - x[i])*(x[i + 1] - x[i]) + (y[i + 1] - y[i])*(y[i + 1] - y[i]);
+		L[i] = sqrt(c) ;
 
 	}
 
-	for (i = 0; i <= n - 1; i++)
+	for (i = 0; i < n - 1; i++)
 	{
 
 		S = S + L[i];
@@ -35,7 +37,7 @@ void main()
 
 	SrArr = S / (n - 1);
 
-	for ( i = 0; i <= n - 1; i++)
+	for ( i = 0; i < n - 1; i++)
 	{
 
 		if (L[i] < SrArr)
@@ -45,7 +47,7 @@ void main()
 
 	}
 
-	R = (br / (n - 1)) * 100;
+	R = (br / (float)(n - 1)) * 100;
 
 	cout << "Procent = " << R << endl;
 
